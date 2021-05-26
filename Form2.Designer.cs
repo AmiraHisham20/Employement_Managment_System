7ad396166120f9b6d7bb80b28f9d4837a677f74b
Sema Ghith <semaghith11@gmail.com>
2021-05-26 23:07:11 +0200

    Delete an Extra ComboBox in Form 3 Rename Label of Grad-State in Form 2

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -55,45 +55,43 @@ namespace SWE_Form1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Franklin Gothic Demi", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(249, 48);
+            this.label1.Location = new System.Drawing.Point(221, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(407, 42);
+            this.label1.Size = new System.Drawing.Size(342, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "Seeker Registering Form";
             // 
             // txt_ssn
             // 
-            this.txt_ssn.Location = new System.Drawing.Point(562, 144);
-            this.txt_ssn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_ssn.Location = new System.Drawing.Point(500, 115);
             this.txt_ssn.Name = "txt_ssn";
-            this.txt_ssn.Size = new System.Drawing.Size(112, 26);
+            this.txt_ssn.Size = new System.Drawing.Size(100, 22);
             this.txt_ssn.TabIndex = 1;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(197, 200);
+            this.label3.Location = new System.Drawing.Point(175, 160);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 25);
+            this.label3.Size = new System.Drawing.Size(53, 20);
             this.label3.TabIndex = 3;
             this.label3.Text = "Name";
             // 
             // txt_name
             // 
-            this.txt_name.Location = new System.Drawing.Point(562, 200);
-            this.txt_name.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_name.Location = new System.Drawing.Point(500, 160);
             this.txt_name.Name = "txt_name";
-            this.txt_name.Size = new System.Drawing.Size(112, 26);
+            this.txt_name.Size = new System.Drawing.Size(100, 22);
             this.txt_name.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(197, 144);
+            this.label2.Location = new System.Drawing.Point(175, 115);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 25);
+            this.label2.Size = new System.Drawing.Size(43, 20);
             this.label2.TabIndex = 5;
             this.label2.Text = "SSN";
             // 
@@ -101,35 +99,33 @@ namespace SWE_Form1
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(197, 252);
+            this.label4.Location = new System.Drawing.Point(175, 202);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(87, 25);
+            this.label4.Size = new System.Drawing.Size(68, 20);
             this.label4.TabIndex = 6;
             this.label4.Text = "Number";
             // 
             // txt_number
             // 
-            this.txt_number.Location = new System.Drawing.Point(562, 252);
-            this.txt_number.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_number.Location = new System.Drawing.Point(500, 202);
             this.txt_number.Name = "txt_number";
-            this.txt_number.Size = new System.Drawing.Size(112, 26);
+            this.txt_number.Size = new System.Drawing.Size(100, 22);
             this.txt_number.TabIndex = 7;
             // 
             // txt_mail
             // 
-            this.txt_mail.Location = new System.Drawing.Point(562, 312);
-            this.txt_mail.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_mail.Location = new System.Drawing.Point(500, 250);
             this.txt_mail.Name = "txt_mail";
-            this.txt_mail.Size = new System.Drawing.Size(112, 26);
+            this.txt_mail.Size = new System.Drawing.Size(100, 22);
             this.txt_mail.TabIndex = 8;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(197, 312);
+            this.label5.Location = new System.Drawing.Point(175, 250);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(52, 25);
+            this.label5.Size = new System.Drawing.Size(40, 20);
             this.label5.TabIndex = 9;
             this.label5.Text = "Mail";
             // 
@@ -137,53 +133,50 @@ namespace SWE_Form1
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(198, 362);
+            this.label6.Location = new System.Drawing.Point(176, 290);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(79, 25);
+            this.label6.Size = new System.Drawing.Size(62, 20);
             this.label6.TabIndex = 10;
             this.label6.Text = "Adress";
             // 
             // txt_adress
             // 
-            this.txt_adress.Location = new System.Drawing.Point(562, 362);
-            this.txt_adress.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_adress.Location = new System.Drawing.Point(500, 290);
             this.txt_adress.Name = "txt_adress";
-            this.txt_adress.Size = new System.Drawing.Size(112, 26);
+            this.txt_adress.Size = new System.Drawing.Size(100, 22);
             this.txt_adress.TabIndex = 11;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(198, 414);
+            this.label7.Location = new System.Drawing.Point(176, 331);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(144, 25);
+            this.label7.Size = new System.Drawing.Size(111, 20);
             this.label7.TabIndex = 12;
             this.label7.Text = "Field of Study";
             // 
             // txt_field_of_study
             // 
-            this.txt_field_of_study.Location = new System.Drawing.Point(562, 414);
-            this.txt_field_of_study.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_field_of_study.Location = new System.Drawing.Point(500, 331);
             this.txt_field_of_study.Name = "txt_field_of_study";
-            this.txt_field_of_study.Size = new System.Drawing.Size(112, 26);
+            this.txt_field_of_study.Size = new System.Drawing.Size(100, 22);
             this.txt_field_of_study.TabIndex = 13;
             // 
             // txt_gradeYear
             // 
-            this.txt_gradeYear.Location = new System.Drawing.Point(562, 472);
-            this.txt_gradeYear.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_gradeYear.Location = new System.Drawing.Point(500, 378);
             this.txt_gradeYear.Name = "txt_gradeYear";
-            this.txt_gradeYear.Size = new System.Drawing.Size(112, 26);
+            this.txt_gradeYear.Size = new System.Drawing.Size(100, 22);
             this.txt_gradeYear.TabIndex = 14;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(197, 473);
+            this.label8.Location = new System.Drawing.Point(175, 378);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(170, 25);
+            this.label8.Size = new System.Drawing.Size(130, 20);
             this.label8.TabIndex = 15;
             this.label8.Text = "Graduation Year";
             // 
@@ -191,27 +184,25 @@ namespace SWE_Form1
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(197, 572);
+            this.label9.Location = new System.Drawing.Point(175, 458);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(211, 25);
+            this.label9.Size = new System.Drawing.Size(163, 20);
             this.label9.TabIndex = 16;
             this.label9.Text = "Years Of Experience";
             // 
             // txt_exp_years
             // 
-            this.txt_exp_years.Location = new System.Drawing.Point(562, 572);
-            this.txt_exp_years.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_exp_years.Location = new System.Drawing.Point(500, 458);
             this.txt_exp_years.Name = "txt_exp_years";
-            this.txt_exp_years.Size = new System.Drawing.Size(112, 26);
+            this.txt_exp_years.Size = new System.Drawing.Size(100, 22);
             this.txt_exp_years.TabIndex = 17;
             // 
             // btn_seeker_reg
             // 
             this.btn_seeker_reg.Font = new System.Drawing.Font("Franklin Gothic Medium", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_seeker_reg.Location = new System.Drawing.Point(385, 624);
-            this.btn_seeker_reg.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_seeker_reg.Location = new System.Drawing.Point(342, 499);
             this.btn_seeker_reg.Name = "btn_seeker_reg";
-            this.btn_seeker_reg.Size = new System.Drawing.Size(119, 38);
+            this.btn_seeker_reg.Size = new System.Drawing.Size(106, 30);
             this.btn_seeker_reg.TabIndex = 18;
             this.btn_seeker_reg.Text = "Register";
             this.btn_seeker_reg.UseVisualStyleBackColor = true;
@@ -219,26 +210,27 @@ namespace SWE_Form1
             // 
             // txt_grade_state
             // 
-            this.txt_grade_state.Location = new System.Drawing.Point(562, 525);
+            this.txt_grade_state.Location = new System.Drawing.Point(500, 420);
+            this.txt_grade_state.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_grade_state.Name = "txt_grade_state";
-            this.txt_grade_state.Size = new System.Drawing.Size(111, 26);
+            this.txt_grade_state.Size = new System.Drawing.Size(99, 22);
             this.txt_grade_state.TabIndex = 19;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(197, 526);
+            this.label10.Location = new System.Drawing.Point(175, 421);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(170, 25);
+            this.label10.Size = new System.Drawing.Size(135, 20);
             this.label10.TabIndex = 15;
-            this.label10.Text = "Graduation Year";
+            this.label10.Text = "Graduation State";
             // 
             // reg_seeker
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(918, 730);
+            this.ClientSize = new System.Drawing.Size(816, 584);
             this.Controls.Add(this.txt_grade_state);
             this.Controls.Add(this.btn_seeker_reg);
             this.Controls.Add(this.txt_exp_years);
@@ -259,7 +251,6 @@ namespace SWE_Form1
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txt_ssn);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "reg_seeker";
             this.Text = "Seeker\'s Data";
             this.Load += new System.EventHandler(this.reg_seeker_Load);
