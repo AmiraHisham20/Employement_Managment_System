33b819599672930b5248157d2b4c866cb2cd84c0
Abdelwahab-Eldesoky <abdelwahab20191702045@cis.asu.edu.eg>
2021-05-27 20:29:55 +0200

    form addVac. and Companyreg. Frontend edit

--- addVacancies.Designer.cs
+++ addVacancies.Designer.cs
@@ -40,80 +40,81 @@ namespace SWE_Form1
             this.rdb_FullTime = new System.Windows.Forms.RadioButton();
             this.rdb_Internship = new System.Windows.Forms.RadioButton();
             this.cmb_companyName = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(51, 92);
+            this.label1.Font = new System.Drawing.Font("Gadugi", 13.8F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(177, 155);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 32);
+            this.label1.Size = new System.Drawing.Size(119, 28);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Job title";
+            this.label1.Text = "Job title :";
             // 
             // txt_vacanciesNo
             // 
-            this.txt_vacanciesNo.Location = new System.Drawing.Point(423, 316);
-            this.txt_vacanciesNo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_vacanciesNo.Location = new System.Drawing.Point(501, 340);
             this.txt_vacanciesNo.Name = "txt_vacanciesNo";
-            this.txt_vacanciesNo.Size = new System.Drawing.Size(195, 26);
+            this.txt_vacanciesNo.Size = new System.Drawing.Size(194, 22);
             this.txt_vacanciesNo.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(43, 225);
+            this.label2.Font = new System.Drawing.Font("Gadugi", 13.8F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(177, 278);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(214, 32);
+            this.label2.Size = new System.Drawing.Size(204, 28);
             this.label2.TabIndex = 0;
-            this.label2.Text = "Company name";
+            this.label2.Text = "Company name :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(43, 154);
+            this.label3.Font = new System.Drawing.Font("Gadugi", 13.8F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(177, 212);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(122, 32);
+            this.label3.Size = new System.Drawing.Size(124, 28);
             this.label3.TabIndex = 0;
-            this.label3.Text = "Job type";
+            this.label3.Text = "Job type :";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(43, 316);
+            this.label4.Font = new System.Drawing.Font("Gadugi", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(177, 335);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(280, 32);
+            this.label4.Size = new System.Drawing.Size(267, 28);
             this.label4.TabIndex = 0;
-            this.label4.Text = "Number of vacancies";
+            this.label4.Text = "Number of vacancies :";
             // 
             // btn_Add
             // 
-            this.btn_Add.Location = new System.Drawing.Point(342, 435);
+            this.btn_Add.Location = new System.Drawing.Point(443, 430);
+            this.btn_Add.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_Add.Name = "btn_Add";
-            this.btn_Add.Size = new System.Drawing.Size(162, 54);
+            this.btn_Add.Size = new System.Drawing.Size(144, 43);
             this.btn_Add.TabIndex = 5;
-            this.btn_Add.Text = "Add ";
+            this.btn_Add.Text = "Add New";
             this.btn_Add.UseVisualStyleBackColor = true;
             this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
             // 
             // txt_JobTitle
             // 
-            this.txt_JobTitle.Location = new System.Drawing.Point(423, 98);
-            this.txt_JobTitle.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_JobTitle.Location = new System.Drawing.Point(501, 160);
             this.txt_JobTitle.Name = "txt_JobTitle";
-            this.txt_JobTitle.Size = new System.Drawing.Size(195, 26);
+            this.txt_JobTitle.Size = new System.Drawing.Size(194, 22);
             this.txt_JobTitle.TabIndex = 1;
             // 
             // rdb_FullTime
             // 
             this.rdb_FullTime.AutoSize = true;
-            this.rdb_FullTime.Location = new System.Drawing.Point(407, 162);
+            this.rdb_FullTime.Location = new System.Drawing.Point(478, 219);
+            this.rdb_FullTime.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.rdb_FullTime.Name = "rdb_FullTime";
-            this.rdb_FullTime.Size = new System.Drawing.Size(97, 24);
+            this.rdb_FullTime.Size = new System.Drawing.Size(86, 21);
             this.rdb_FullTime.TabIndex = 6;
             this.rdb_FullTime.TabStop = true;
             this.rdb_FullTime.Text = "Full Time";
@@ -122,9 +123,10 @@ namespace SWE_Form1
             // rdb_Internship
             // 
             this.rdb_Internship.AutoSize = true;
-            this.rdb_Internship.Location = new System.Drawing.Point(577, 162);
+            this.rdb_Internship.Location = new System.Drawing.Point(629, 219);
+            this.rdb_Internship.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.rdb_Internship.Name = "rdb_Internship";
-            this.rdb_Internship.Size = new System.Drawing.Size(105, 24);
+            this.rdb_Internship.Size = new System.Drawing.Size(91, 21);
             this.rdb_Internship.TabIndex = 7;
             this.rdb_Internship.TabStop = true;
             this.rdb_Internship.Text = "Internship";
@@ -133,18 +135,30 @@ namespace SWE_Form1
             // cmb_companyName
             // 
             this.cmb_companyName.FormattingEnabled = true;
-            this.cmb_companyName.Location = new System.Drawing.Point(415, 242);
+            this.cmb_companyName.Location = new System.Drawing.Point(501, 283);
+            this.cmb_companyName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmb_companyName.Name = "cmb_companyName";
-            this.cmb_companyName.Size = new System.Drawing.Size(202, 28);
+            this.cmb_companyName.Size = new System.Drawing.Size(194, 24);
             this.cmb_companyName.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Agency FB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(312, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(369, 50);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Adding New Vacancy Form";
             // 
             // AddVacancies
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(126)))), ((int)(((byte)(144)))), ((int)(((byte)(250)))));
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(988, 580);
+            this.ClientSize = new System.Drawing.Size(992, 589);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.cmb_companyName);
             this.Controls.Add(this.rdb_Internship);
             this.Controls.Add(this.rdb_FullTime);
@@ -156,7 +170,6 @@ namespace SWE_Form1
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "AddVacancies";
             this.Text = "Add Vacancies";
             this.Load += new System.EventHandler(this.AddVacancies_Load);
@@ -177,5 +190,6 @@ namespace SWE_Form1
         private System.Windows.Forms.RadioButton rdb_FullTime;
         private System.Windows.Forms.RadioButton rdb_Internship;
         private System.Windows.Forms.ComboBox cmb_companyName;
+        private System.Windows.Forms.Label label5;
     }
 }
