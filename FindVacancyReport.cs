71ad568131e0755e131fab5bb76cc31a1c615a61
AmiraHisham20 <you@example.com>
2021-06-09 01:36:33 +0200

    Crystal Dynamic

--- FindVacancyReport.cs
+++ FindVacancyReport.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CrystalDecisions.Shared;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
 
 namespace SWE_Form1
 {
@@ -15,6 +17,8 @@ namespace SWE_Form1
     public partial class Report2 : Form
     {
         CrystalReport4 CR2;
+        OracleConnection conn;
+        string ordb = "Data Source=PESO;User Id=hr;Password=hr;";
         public Report2()
         {
             InitializeComponent();
@@ -22,9 +26,24 @@ namespace SWE_Form1
 
         private void Report2_Load(object sender, EventArgs e)
         {
+            conn = new OracleConnection(ordb);
+            conn.Open();
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select distinct JOB_NAME from jobvacancies";
+
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                cmb_report2.Items.Add(dr[0]);
+            }
+
+            dr.Close();
+
             CR2 = new CrystalReport4();
-            foreach (ParameterDiscreteValue V2 in CR2.ParameterFields[0].DefaultValues)
-                cmb_report2.Items.Add(V2.Value);
+            //foreach (ParameterDiscreteValue V2 in CR2.ParameterFields[0].DefaultValues)
+            //    cmb_report2.Items.Add(V2.Value);
         }
 
         private void btn_report2_Click(object sender, EventArgs e)
